5195886a757204bce1aaba69c49556ac5b3f45ab
José Carlos Alves <4664826+zedomus@users.noreply.github.com>
2026-01-15 22:53:22 +0000

    Acrescentado código para testar

--- Program.cs
+++ Program.cs
@@ -13,6 +13,13 @@ namespace Calculos
             Console.WriteLine("Aplicação Cálculos");
             Console.WriteLine($"4 + 2 = {Aritmetica.Somar(4, 2)}");
             Console.WriteLine($"4 - 2 = {Aritmetica.Subtrair(4, 2)}");
+
+            double t1 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.CelsiusFahrenheit, 36);
+            double t2 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.FahrenheitCelsius, 100);
+
+            Console.WriteLine($"36º Celsius = {t1}º Fahrenheit");
+            Console.WriteLine($"100º Fahrenheit = {t2}º Celsius");
+
             Console.ReadKey(); 
         }
     }
